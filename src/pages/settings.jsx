358a6126b0c1029f01e0f36a169ef1748c73e830
Alex Raduca <alexandru.raduca@gmail.com>
2022-12-14 23:39:19 +0200

    settings update

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
-import { Page, Navbar, Block, List, ListItem, ListInput, ListButton, Icon, useStore } from 'framework7-react';
-import { property } from 'lodash';
+import React, { useState, useEffect } from 'react';
+import { Page, Navbar, Block, List, ListItem, ListInput, Stepper, ListButton, Icon, useStore, Row, Col } from 'framework7-react';
 
 const SettingsPage = () => {
     const settings = useStore('settings');
     const properties = useStore('properties');
+    const [editProperties, setEditProperties] = useState(false)
+    const [editChannels, setEditChannels] = useState(false)
+    const [editCategories, setEditCategories] = useState(false)
     console.log({ properties })
 
     return (
@@ -16,15 +18,16 @@ const SettingsPage = () => {
                         <ListItem >
                             <h3 slot="header">Properties</h3>
                             <div inset style={{ display: 'flex', flexDirection: 'row', gap: 4 }}>
-                                <ListButton small raised fill><Icon material="save" /></ListButton>
-                                <ListButton small raised fill><Icon material="edit" /></ListButton>
-                                <ListButton small raised fill><Icon material="add" /></ListButton>
+                                {editProperties && <ListButton ><Icon material="save" /></ListButton>}
+                                {editProperties && <ListButton><Icon material="add" /></ListButton>}
+                                {editProperties || <ListButton onClick={() => setEditProperties(true)}><Icon material="edit" /></ListButton>}
                             </div>
                         </ListItem>
                         {properties.map((item, index) => (
-                            <ListInput key={item.id} name={"property." + index} defaultValue={item.Name} >
+                            <ListInput key={item.id} name={"property." + index} readonly={editProperties} defaultValue={item.Name} >
                             </ListInput>
                         ))}
+
                     </List>
                 </form>
                 <form>
@@ -32,12 +35,12 @@ const SettingsPage = () => {
                         <ListItem >
                             <h3 slot="header">Booking channels</h3>
                             <div inset style={{ display: 'flex', flexDirection: 'row', gap: 4 }}>
-                                <ListButton small raised fill><Icon material="save" /></ListButton>
-                                <ListButton small raised fill><Icon material="edit" /></ListButton>
+                                {editChannels && <ListButton small raised fill><Icon material="save" /></ListButton>}
+                                {editChannels || <ListButton small raised fill><Icon material="edit" /></ListButton>}
                                 <ListButton small raised fill><Icon material="add" /></ListButton>
                             </div>
                         </ListItem>
-                        {settings.channels.map((item, index) => <ListInput key={item} name={"channel." + index} defaultValue={item} />)}
+                        {settings.channels.map((item, index) => <ListInput key={item} name={"channel." + index} readonly={editChannels} defaultValue={item} />)}
                     </List>
                 </form>
                 <form>
@@ -45,12 +48,12 @@ const SettingsPage = () => {
                         <ListItem >
                             <h3 slot="header">Expense categories</h3>
                             <div inset style={{ display: 'flex', flexDirection: 'row', gap: 4 }}>
-                                <ListButton small raised fill><Icon material="save" /></ListButton>
-                                <ListButton small raised fill><Icon material="edit" /></ListButton>
+                                {editCategories && <ListButton small raised fill><Icon material="save" /></ListButton>}
+                                {editCategories || <ListButton small raised fill><Icon material="edit" /></ListButton>}
                                 <ListButton small raised fill><Icon material="add" /></ListButton>
                             </div>
                         </ListItem>
-                        {settings.expenseCategories.map((item, index) => <ListInput key={item} name={"expenseCategory." + index} defaultValue={item} />)}
+                        {settings.expenseCategories.map((item, index) => <ListInput key={item} name={"expenseCategory." + index} readonly={editCategories} defaultValue={item} />)}
                     </List>
                 </form>
             </Block>
