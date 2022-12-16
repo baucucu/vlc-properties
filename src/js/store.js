22a8a9c82b73fb9169dd4f87aea30618db85ac14
Alex Raduca <alexandru.raduca@gmail.com>
2022-12-16 10:26:05 +0200

    update

--- src/js/store.js
+++ src/js/store.js
@@ -275,7 +275,7 @@ const store = createStore({
               "Rent": currency(data.rent) / 100,
               "Deposit": currency(data.deposit) / 100,
               "Check in": dayjs(data.checkIn).format('YYYY-MM-DD'),
-              "Check out": dayjs(data.checkOut).format('YYYY-MM-DD'),
+              "Check out": dayjs(data.checkOut).format('YYYY-MM-DD')
             },
             typecast: true
           }
